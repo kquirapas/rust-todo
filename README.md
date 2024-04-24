dcf7de9f909c25d7e10e58939c3282c28f46176a
Kristian Quirapas <kmquirapas@gmail.com>
2024-04-24 14:12:51 +0800

    add: templating

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Rust reference repository for juniors engineer
+# Rust reference repository for Web3 Philippines interns / junior engineers
+
+## What you can learn from this repository
+
+1. Hexagonal Architecture (Ports and Adapters) used for Persistence and Request.
+2. Handling migrations and CRUD operations with sqlx
+3. PostgreSQL
+4. Telemetry/Tracing/Monitoring/Instrumentation
+
+# Technical Documentation
+
+## Page Routes
+
+### Authentiation
+/
+/signin
+/signup
+/signout
+
+### CRUD
+/create
+/read
+/update
+/delete
--- migrations/20240424053134_init.up.sql
+++ migrations/20240424053134_init.up.sql
@@ -1,1 +1,48 @@
 -- Add up migration script here
+create table accounts (
+	id uuid not null default gen_random_uuid(),
+	email varchar(150) not null unique,
+	password bytea not null,
+	salt bytea not null,
+	username varchar(16) not null unique,
+	about varchar(255),
+	primary key (id)
+);
+
+create table tasks (
+	id uuid default gen_random_uuid(),
+	description text,
+	created_by uuid,
+	created_at timestamp not null default current_timestamp,
+	primary key (id),
+	foreign key (created_by) references accounts(id)
+);
+
+create table roles (
+	id uuid not null default gen_random_uuid() unique,
+	name varchar(16) not null unique,
+	description varchar(255) not null
+);
+
+create table permissions (
+	id uuid not null default gen_random_uuid() unique,
+	name varchar(16) not null unique,
+	description varchar(255) not null
+);
+
+create table accounts_roles (
+	account_id uuid,
+	role_id uuid,
+	primary key (account_id, role_id),
+	foreign key (account_id) references accounts(id),
+	foreign key (role_id) references roles(id)
+);
+
+create table roles_permissions (
+	role_id uuid,
+	permissions_id uuid,
+	primary key (role_id, permissions_id),
+	foreign key (role_id) references roles(id),
+	foreign key (permissions_id) references permissions(id)
+);
+
